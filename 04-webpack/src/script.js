f869f4c6bfb160379dfac6eaf2cc7cb12d7022b3
Seth-McKilla <seth@endevrs.dev>
2022-02-05 15:31:41 -0500

    Finish: Lesson 04 - Webpack

--- 04-webpack/src/script.js
+++ 04-webpack/src/script.js
@@ -1,1 +1,33 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
+
+// Setup scene
+const scene = new THREE.Scene();
+
+// Create object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Add object to scene
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Add camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
